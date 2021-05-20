2a3e6de3f84cceb6eba802443a91f2984d1353f2
satriobhirawa <satrio.bhirawa@yahoo.com>
2021-05-20 10:11:43 +0200

    example of application.properties on .env file, add student into database, added file to gitignore

--- src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
+++ src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
@@ -3,6 +3,9 @@ package com.bhirawa.demoproduct;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemoProductApplication {
@@ -10,5 +13,18 @@ public class DemoProductApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProductApplication.class, args);
 	}
-	CommandLineRunner commandLineRunner()
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+		return args -> {
+			Student bhirawa = new Student(
+				"Bhirawa", "Satrio", "bhir@yahoo.com", 29
+			);
+			Student satrio = new Student(
+					"Satrio", "Nugroho", "satr@yahoo.com", 30
+			);
+			studentRepository.saveAll(List.of(bhirawa,satrio));
+
+			//find student by ID
+		};
+	}
 }
