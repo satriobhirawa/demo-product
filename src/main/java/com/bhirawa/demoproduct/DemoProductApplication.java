3e70648dec45f9214aa1c3290933f92e9cde7601
satriobhirawa <satrio.bhirawa@yahoo.com>
2021-05-21 20:48:49 +0200

    added pagination

--- src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
+++ src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
@@ -19,14 +21,28 @@ public class DemoProductApplication {
 		return args -> {
 
 			generateRandomStudent(studentRepository);
-			//Sort by firstname ASC
-			//Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
-			//OR
-			Sort sort = Sort.by("firstName").ascending()
-					.and(Sort.by("age").descending());
-			studentRepository.findAll(sort).forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+			//sorting(studentRepository);
+
+			//pagination + sorting
+			//example page 0 : first page, size of data per page 5
+			PageRequest pageRequest = PageRequest.of(0,
+					5,
+					Sort.by("firstName").ascending());
+			Page<Student> page = studentRepository.findAll(pageRequest);
+			System.out.println(page);
+
 		};
 	}
+
+	private void sorting(StudentRepository studentRepository) {
+		//Sort by firstname ASC
+		//Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+		//OR
+		Sort sort = Sort.by("firstName").ascending()
+				.and(Sort.by("age").descending());
+		studentRepository.findAll(sort).forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+	}
+
 	//use faker to populate the database
 	private void generateRandomStudent(StudentRepository studentRepository) {
 		Faker faker = new Faker();
