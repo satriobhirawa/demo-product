4970d7475b444eb6439baffa618e651334c446ee
satriobhirawa <satrio.bhirawa@yahoo.com>
2021-05-21 17:37:13 +0200

    native query and custom query added

--- src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
+++ src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
@@ -13,20 +13,38 @@ public class DemoProductApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProductApplication.class, args);
 	}
+
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
 		return args -> {
-			System.out.println("Add student");
 			Student bhirawa = new Student(
 				"Bhirawa", "Satrio", "bhir@yahoo.com", 29
 			);
 			Student satrio = new Student(
 					"Satrio", "Nugroho", "satr@yahoo.com", 30
 			);
-			studentRepository.saveAll(List.of(bhirawa,satrio));
+			Student nugroho = new Student(
+					"Nugroho", "Zakaria", "nugr@yahoo.com", 31
+			);
+			studentRepository.saveAll(List.of(bhirawa,satrio,nugroho));
+			studentRepository
+					.findStudentByEmail("bhir@yahoo.com")
+					.ifPresentOrElse(System.out::println, ()-> System.out.println("Student with that email not found"));
 
-			//find student by ID
-			System.out.println("Find by ID");
+			//find by first name and age with custom query
+			studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqual(
+					"Bhirawa",
+					28
+			).forEach(System.out::println);
+
+			//find by first name and age with native query
+			studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+					"Satrio",
+					29
+			).forEach(System.out::println);
+
+		//find student by ID
+/*			System.out.println("Find by ID");
 			studentRepository.findById(2L).
 					ifPresentOrElse(System.out::println, () -> {
 					System.out.println("Student with id 2 not found!");
@@ -35,7 +53,7 @@ public class DemoProductApplication {
 			//find all student
 			System.out.println("Find all student");
 			List <Student> students = studentRepository.findAll();
-			students.forEach(System.out::println);
+			students.forEach(System.out::println);*/
 
 			//delete student
 			/*studentRepository.deleteById(1L);
--- src/main/java/com/bhirawa/demoproduct/StudentRepository.java
+++ src/main/java/com/bhirawa/demoproduct/StudentRepository.java
@@ -1,7 +1,23 @@
 package com.bhirawa.demoproduct;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository <Student, Long> {
+    //not a must, but nice to have to gain full control of query
+    //?1 means only passing one param and at first position on the method below
+    @Query("SELECT s FROM Student s WHERE s.email =?1")
+    Optional<Student> findStudentByEmail(String email);
+
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqual(
+            String firstName, Integer age);
+
+    @Query(value = "SELECT * FROM student WHERE first_name = ?1 AND age >= ?2", nativeQuery = true)
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+            String firstName, Integer age);
 
 }
