7be94c71e2a280906a50f95dd3b416d6a73e595d
satriobhirawa <satrio.bhirawa@yahoo.com>
2021-05-21 20:04:30 +0200

    installed faker(dummy data) and sorting added

--- src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
+++ src/main/java/com/bhirawa/demoproduct/DemoProductApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class DemoProductApplication {
@@ -16,19 +17,28 @@ public class DemoProductApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
 		return args -> {
-			//use faker to populate the database
-		Faker faker = new Faker();
-			for (int i = 0; i < 20 ; i++) {
-				String firstName = faker.name().firstName();
-				String lastName = faker.name().lastName();
-				String email = String.format("%s.%s@yahoo.com", firstName, lastName);
-				Student student = new Student(firstName,
-						lastName,
-						email,
-						faker.number().numberBetween(17,55));
-				studentRepository.save(student);
-			}
 
+			generateRandomStudent(studentRepository);
+			//Sort by firstname ASC
+			//Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+			//OR
+			Sort sort = Sort.by("firstName").ascending()
+					.and(Sort.by("age").descending());
+			studentRepository.findAll(sort).forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
 		};
+	}
+	//use faker to populate the database
+	private void generateRandomStudent(StudentRepository studentRepository) {
+		Faker faker = new Faker();
+		for (int i = 0; i < 20 ; i++) {
+			String firstName = faker.name().firstName();
+			String lastName = faker.name().lastName();
+			String email = String.format("%s.%s@yahoo.com", firstName, lastName);
+			Student student = new Student(firstName,
+					lastName,
+					email,
+					faker.number().numberBetween(17,55));
+			studentRepository.save(student);
+		}
 	}
 }
